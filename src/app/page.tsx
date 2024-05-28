e00d135992db40ca438e185252591290c13b5fb5
sahil7886 <shaikh28@purdue.edu>
2024-05-28 17:50:39 +0530

    frontend hi done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm duration-550 text-zinc-500 hover:text-zinc-300"
             >
               {item.name}
             </Link>
@@ -33,12 +33,12 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="mb-6 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+      <h1 className="mb-6 z-10 text-4xl text-transparent duration-1100 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
         Sahil Shaikh
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-lg text-zinc-400 duration-500 hover:text-transparent">
+        <h2 className="text-lg text-zinc-400 duration-550 hover:text-transparent">
           I'm studying Computer Science at Purdue University.
         </h2>
       </div>
